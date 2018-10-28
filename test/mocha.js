6aced923e80d1667bc58c982462d2014920b7236
lipeihan <291630202@qq.com>
2018-10-28 12:01:43 +0800

    feat: 引入 mocha

--- test/mocha.js
+++ test/mocha.js
@@ -1,9 +1,35 @@
 const { sum, mul } = require('../src/math');
 const { should, expect, assert } = require('chai');
 
+// should();
+// sum(2, 3).should.equal(5);
+
+// expect(sum(2,  3)).to.equal(5);
+
+// assert.equal(mul(2, 3), 6);
+
 should();
-sum(2, 3).should.equal(5);
 
-expect(sum(2,  3)).to.equal(5);
+describe('#math', () => {
+    describe('sum', () => {
+        it('should return 5 when sum(2, 3)', () => {
+            sum(2, 3).should.equal(5);
+        });
+    });
 
-assert.equal(mul(2, 3), 6);
+    describe('sum', () => {
+        it('should return -1 when sum(2, -3)', () => {
+            sum(2, -3).should.equal(-1);
+        });
+
+        it('should return -1 when sum(2, -3)', () => {
+            sum(2, -3).should.equal(-1);
+        });
+    });
+
+    describe('mul', () => {
+        it('should return -6 when mul(2, -3)', () => {
+            mul(2, -3).should.equal(-6);
+        });
+    });
+});
